3a878997a6764f482258c7a73ad5e256151b6dab
DenzellM-2054872 <denzell.mgbokwere@student.uhasselt.be>
2021-03-04 21:57:49 +0100

    people can now move again after a failed move

--- Chess/Chess/main.cpp
+++ Chess/Chess/main.cpp
@@ -18,13 +18,16 @@ int main() {
 				aiB.playMove();
 		}
 		else {
+			int test = 1;
+			while (test != 0){
 			int x1, x2, y1, y2;
 			std::cout << "Player " << (mainGame.getTurn() % 2) + 1 << "\n";
 			std::cout << "Enter the position of the piece you want to move: ";
 			std::cin >> x1 >> y1;
 			std::cout << "Enter the position of the place you want to move to: ";
 			std::cin >> x2 >> y2;
-			mainGame.movePiece(x1, y1, x2, y2);
+			test = mainGame.movePiece(x1, y1, x2, y2);
+			}
 		}
 		mainGame.print();
 		mainGame.checkEnd();
