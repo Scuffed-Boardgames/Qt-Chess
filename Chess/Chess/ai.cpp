1dae748d08cd135de8aaad26ae9a7fc614fcce58
DenzellM-2054872 <denzell.mgbokwere@student.uhasselt.be>
2021-03-18 15:50:57 +0100

    made the ai work with the new implementations

--- Chess/Chess/ai.cpp
+++ Chess/Chess/ai.cpp
@@ -8,85 +8,27 @@ Ai::Ai(Colour colour, Board* board) : m_colour{ colour }, m_board{ board }
 {}
 
 int Ai::playMove() {
-	bool test = checkLastRow();
-	if (test)
-		return 0;
-	test = checkTake();
-	if (test)
-		return 0;
-	int check = movePiece();
-	if (check == 0)
-		return 0;
-	return 1;
-}
-
-bool Ai::checkLastRow() {
-	std::vector<Pawn> pawns = m_board->getPawn(m_colour);
-	size_t len = pawns.size();
-	for (int i = 0; i < len; ++i) {
-		int backRow = 2;
-		if (m_colour == Colour::white)
-			backRow = 7;
-		if (pawns.at(i).getY() == backRow){
-			int pawnX = pawns.at(i).getX();
-			int pawnY = pawns.at(i).getY();
-			int test = m_board->checkMove(pawnX, pawnY, pawnX, pawnY + (int)m_colour, m_colour);
-			if (test == 0) {
-				m_board->makeMove(pawnX, pawnY, pawnX, pawnY + (int)m_colour, m_colour);
-				return true;
-			}
-		}
-	}
-	
-	return false;
-}
-
-bool Ai::checkTake() {
-	std::vector<Pawn> pawns = m_board->getPawn(m_colour);
-	size_t len = pawns.size();
-	for (int i = 0; i < len; ++i) {
-		int x = pawns.at(i).getX();
-		int y = pawns.at(i).getY();
-		int test = m_board->checkMove(x, y, x + 1, y + (int)m_colour, m_colour);
-		if (test <= 0) {
-			m_board->makeMove(x, y, x + 1, y + (int)m_colour, m_colour);
-			return true;
-		}
-		test = m_board->checkMove(x, y, x - 1, y + (int)m_colour, m_colour);
-		if (test <= 0) {
-			m_board->makeMove(x, y, x - 1, y + (int)m_colour, m_colour);
-			return true;
-		}
-	}
-	return false;
-}
-
-int Ai::movePiece() {
 	int chance = rand() % 10;
-	std::vector<Pawn> pawns = m_board->getPawn(m_colour);
+	std::vector<Piece> pieces = m_board->getPieces(m_colour);
 	int c = 0;
-	while (true){
-		int pawnNr = rand() % pawns.size();
-		int pawnX = pawns.at(pawnNr).getX();
-		int pawnY = pawns.at(pawnNr).getY();
-		if ((chance > 3) && !(pawns.at(pawnNr).hasMoved())) {
-			int test = m_board->checkMove(pawnX, pawnY, pawnX, pawnY + 2 * (int)m_colour, m_colour);
-			if (test <= 0) {
-				m_board->makeMove(pawnX, pawnY, pawnX, pawnY + 2 * (int)m_colour, m_colour);
+	bool tried = false;
+	while (true) {
+		tried = false;
+		c = 0;
+		int pieceNr = rand() % pieces.size();
+		int pieceX = pieces.at(pieceNr).getX();
+		int pieceY = pieces.at(pieceNr).getY();
+		while (!tried) {
+			int x = (rand() % 8) + 1;
+			int y = (rand() % 8) + 1;
+			int test = m_board->checkMove(pieceX, pieceY, x, y, m_colour);
+			if (test >= 0) {
+				m_board->makeMove(pieceX, pieceY, x, y, m_colour);
 				return 0;
 			}
-		}
-		else {
-			int test = m_board->checkMove(pawnX, pawnY, pawnX, pawnY + (int)m_colour, m_colour);
-			if (test <= 0) {
-				m_board->makeMove(pawnX, pawnY, pawnX, pawnY + (int)m_colour, m_colour);
-				return 0;
-			}
-		}
-		++c;
-		if (c > 100) {
-			std::cout << "coulnd't find a valid move\n";
-			return 1;
+			++c;
+			if (c > 100)
+				tried = true;
 		}
 	}
 }
--- Chess/Chess/ai.h
+++ Chess/Chess/ai.h
@@ -9,9 +9,6 @@ public:
 	int playMove(); // The order in which the ai checks moves (Denzell Mgbokwere)
 
 private:
-	bool checkLastRow(); // The ai checks if it can win this move (Denzell Mgbokwere)
-	bool checkTake(); // The ai checks if it can take another pawn (Denzell Mgbokwere)
-	int movePiece(int chance); // If the ai cant do anything usefull it will move a random pawn (Denzell Mgbokwere)
 	Colour m_colour;
 	Board* m_board;
 };
