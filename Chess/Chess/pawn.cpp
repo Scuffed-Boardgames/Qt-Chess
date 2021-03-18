7745d2de0903f1dda5c004fd7c7c53321ef34a14
DenzellM-2054872 <denzell.mgbokwere@student.uhasselt.be>
2021-03-18 12:27:04 +0100

    made pawn use the piece class

--- Chess/Chess/pawn.cpp
+++ Chess/Chess/pawn.cpp
@@ -2,115 +2,27 @@
 #include "pawn.h"
 #include <iostream>
 
-Pawn::Pawn(int x, int y, Colour colour) : m_x(x), m_y(y), m_colour(colour), m_hasMoved(false), m_hasHopped(false)
+Pawn::Pawn(int x, int y, Colour colour) : Piece(x, y, colour), m_hasMoved(false), m_hasHopped(false)
 {}
 
-bool Pawn::checkBounds(int x, int y) {
-	if (x > 8 || y > 8) {
-		return false;
-	}
-	else if (x < 1 || y < 1) {
-		return false;
-	}
-	return true;
-}
-
 int Pawn::checkMove(int x, int y) {
-	if (!checkBounds(x, y))
+	if (Piece::checkMove(x, y) == 2)
 		return 2;
 
-	if (m_colour == Colour::white) {
-		if (m_x == x && m_y + 1 == y) {
-			return 0;
-		}
-		else if (m_x == x && m_y + 2 == y && !m_hasMoved) {
-			return -2;
-		}
+	if (getX() == x && getY() + (int)getColour() == y) {
+		return 0;
 	}
-	else{
-		if (m_x == x && m_y - 1 == y) {
-			return 0;
-		}
-		else if (m_x == x && m_y - 2 == y && !m_hasMoved) {
-			return -2;
-		}
+
+	if ((getX() == x + 1 || getX() == x - 1) && getY() + (int)getColour() == y) {
+		return 0;
+	}
+
+	else if (getX()== x && getY() + 2 * (int)getColour() == y && !m_hasMoved) {
+		return -2;
 	}
 
 	return 1;
 }
-
-int Pawn::checkTake(int x, int y) {
-	if (!checkBounds(x, y)) {
-		return 2;
-	}
-	if (m_colour == Colour::white) {
-		if ((m_x == x + 1 || m_x == x - 1) && m_y + 1 == y) {
-			return -1;
-		}
-	}
-	else{
-		if ((m_x == x + 1 || m_x == x - 1) && m_y - 1 == y) {
-			return -1;
-		}
-	}
-	return 1;
-}
-
-int Pawn::makeMove(int x, int y) {
-	switch (checkMove(x, y)){
-	case(-2):
-		m_x = x;
-		m_y = y;
-		m_hasMoved = true;
-		m_hasHopped = true;
-		return 0;
-	case(0):
-		m_x = x;
-		m_y = y;
-		m_hasMoved = true;
-		return 0;
-	case(1):
-		std::cout << "inserted move is illegal!\n";
-		return 1;
-	case(2):
-		std::cout << "inserted value(s) are too small/large!\n";
-		return 2;
-	default:
-		return 99;
-	}
-}
-
-int Pawn::makeTake(int x, int y){
-	switch (checkTake(x, y)) {
-	case(-1):
-		m_x = x;
-		m_y = y;
-		m_hasMoved = true;
-		return -1;
-	case(1):
-		std::cout << "inserted move is illegal!\n";
-		return 1;
-	case(2):
-		std::cout << "inserted value(s) are too small/large!\n";
-		return 2;
-	default:
-		return 99;
-	}
-}
-
-
-Colour Pawn::getColour(){
-	return m_colour;
-}
-
-int Pawn::getX(){
-	return m_x;
-}
-
-int Pawn::getY(){
-	return m_y;
-}
-
 
 bool Pawn::hasMoved(){
 	return m_hasMoved;
--- Chess/Chess/pawn.h
+++ Chess/Chess/pawn.h
@@ -3,30 +3,19 @@
 #define PAWN_H
 
 #include "colour.h"
+#include "piece.h"
 
-class Pawn{
+class Pawn : public Piece {
 public:
-
-	Pawn() = default;
 	Pawn(int x, int y, Colour color); // Makes a pawn on given position and can be black or white
 
 	int checkMove(int x, int y); // Checks a move and returns if it is valid(0,-2) or invalid(>0)
-	int checkTake(int x, int y); // Checks a take move and returns if it is valid(-1) or invalid(>0)
-	int makeMove(int x, int y); // Moves the pawn to the given destination if it is a valid move
-	int makeTake(int x, int y); // Checks if the take move is valid and executes it 
 	
-	int getX(); //getter for the x coordinate
-	int getY(); //getter for the y coordinate
 	bool hasMoved(); //getter for the hasmoved bool
-	Colour getColour(); //getter for colour
 
 	bool m_hasHopped;
 
 private:
-	bool checkBounds(int x, int y); // Looks if the given coordinate is a value on the board
-	int m_x;
-	int m_y;
-	Colour m_colour;
 	bool m_hasMoved;
 };
 #endif 
