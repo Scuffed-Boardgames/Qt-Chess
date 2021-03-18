9d6930d72d05b3b78fc724c2530cba6c3164b8bd
Bernd U <berndu2002@gmail.com>
2021-03-18 12:05:22 +0100

    Changed code for the better
    Removed redundant bool and merged 3 functions into 1

--- Chess/Chess/player.h
+++ Chess/Chess/player.h
@@ -7,15 +7,12 @@ public:
 	Player() = default;
 	Player(Colour colour); //Player constructor per colour
 	bool isWhite();
-	bool getWon(); //Returns if the players has won or not
 	bool isAi();
-	void giveWon(); //Changes hasWon to true
 	void setAi();
 
 private:
 	bool m_isAi;
 	Colour m_colour;
-	bool m_hasWon;
 };
 
 #endif 
