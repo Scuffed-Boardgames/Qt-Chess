5e702f026f1f1ebbf933540c5969e61f09210bbe
DenzellM-2054872 <denzell.mgbokwere@student.uhasselt.be>
2021-03-20 14:55:34 +0100

    added documentation

--- Chess/Chess/rook.h
+++ Chess/Chess/rook.h
@@ -6,8 +6,8 @@
 class Rook: public Piece {
 public:
 	Rook(int x, int y, Colour colour);
-	int checkMove(int x2, int y2);
-	char getName();
+	int checkMove(int x2, int y2); // checks if a move is a legal move for this piece
+	char getName(); // returns the starting letter for a piece (in this case 'R') 
 };
 
 #endif 
