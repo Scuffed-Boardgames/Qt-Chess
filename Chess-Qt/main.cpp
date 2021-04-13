95b6b2972add5b07dfcebe0313714adda6865aa7
Bernd U <berndu2002@gmail.com>
2021-04-13 21:19:03 +0200

    Added removal of pieces

--- Chess-Qt/main.cpp
+++ Chess-Qt/main.cpp
@@ -7,8 +7,10 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     Game k;
+    Board* board = k.giveBoard();
     chess w(nullptr, k.giveBoard());
-    chess::connect(w.getScene(),&CustomGraphics::madeMove,&w, &chess::moveMade);
+    chess::connect(w.getScene(), &CustomGraphics::madeMove, &w, &chess::moveMade);
+    chess::connect(board, &Board::removedPiece, &w, &chess::removePiece);
     w.show();
     return a.exec();
 
